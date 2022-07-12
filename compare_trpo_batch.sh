9d837e4d0b3902f755daf290615bb032037a10b4
SonSang <shh1295@gmail.com>
2022-07-11 20:00:37 -0400

    fixed batch script

--- compare_trpo_batch.sh
+++ compare_trpo_batch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-NUM_PROCS=4
+NUM_PROCS=31
 
 for ((c=0; c<$NUM_PROCS; c++))
 do
-    OMP_NUM_THREADS=1 nohup python compare_ppo.py --env Swimmer --n_steps 1000 --n_eval_steps 1000 --pe --no-record & sleep 3
+    OMP_NUM_THREADS=1 nohup python compare_trpo.py --env Swimmer --n_steps 1000_000 --n_eval_steps 10000 --pe --no-record & sleep 1
 done
